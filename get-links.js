1b520c7e9eae9300612fa122f33aa97e6b44b1d2
GuilhermeOliveiraCasagrande <32659090+GuilhermeOliveiraCasagrande@users.noreply.github.com>
2020-09-08 18:56:36 -0300

    Colocando projeto no github

---
+++ get-links.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch() //Roda o browser
+    const page = await browser.newPage() //Abre uma página
+    await page.goto('https://www.youtube.com/') //Faz a página ir para um endereço
+
+    // execute standard javascript in the context of the page.
+    let links = await page.$$eval( //Procura na página
+        'a#video-title-link', //Os elementos como se fosse css
+        links => links.map( //Pega os elementos e faz alguma coisa, e retorna o resultado
+            link => `${link.href}` //Pega o atributo do elemento
+            ).slice(0, 10)
+    )
+    links = links.filter(link => !link.split("/").includes('playlist'))
+    console.log(links)
+    await page.close() //Fecha a página
+    await browser.close() //Fecha o navegador
+})()
