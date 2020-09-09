d224cfa406ea425ca1a1a533809fd1c4623eccbe
Guilherme Oliveira Casagrande <guiolicasa@gmail.com>
2020-09-08 22:36:30 -0300

    Comentando o código para entender melhor

--- search-links.js
+++ search-links.js
@@ -1,17 +1,21 @@
 const puppeteer = require('puppeteer')
 try {
     (async () => {
-        const browser = await puppeteer.launch()
-        const page = await browser.newPage()
+        const browser = await puppeteer.launch() //Roda  navegador
+        const page = await browser.newPage() //Abre uma página
 
-        await page.goto('https://youtube.com')
-        await page.type("input#search", "helltaker ost remix")
-        await page.click("button#search-icon-legacy")
-        await page.reload()
-        const videos = await page.$$eval("a#video-title", anchors => anchors.map(anchor => anchor.href))
-        console.log(page.url())
+        await page.goto('https://youtube.com') //Acessa esse site
+        await page.type("input#search", "helltaker ost remix") //Vai no elemento e escreve algo
+        await page.click("button#search-icon-legacy") //Clica um elemento
+        await page.reload() //Recarrega a página
+
+        const videos = await page.$$eval("a#video-title", //Pega todos os elementos que achar e coloca em um array na função
+            anchors => //Pega os elementos
+                anchors.map( //Transforma em outro array
+                    anchor => anchor.href)) //Um array de endereços, e coloca na variável
+        console.log(page.url()) //Mosta o url da página
         console.log(videos)
-        browser.close()
+        browser.close() //Fecha o navegador
     })()
 } catch (err) {
     console.error(err)
